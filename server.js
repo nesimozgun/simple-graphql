00b1ce5e63643844aee4440cde49d4892859f57a
nesimozgun <ozgunnesim@gmail.com>
2024-10-03 18:02:54 +0300

    update server.js

--- server.js
+++ server.js
@@ -133,4 +133,6 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
-app.listen(5001, () => console.log('Server running on port 5001'));
+app.listen(5001, () => 
+  console.log('Server running on port 5001, navigate to http://localhost:5001/graphql')
+);
